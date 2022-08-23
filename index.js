958731da4bda9a44cf11b968b25fa744e6a088a8
me edl <103306349+Edlme@users.noreply.github.com>
2022-08-23 06:25:29 +0000

    list/view

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var data = require('./data/test.json');
 
 app.set('view engine','ejs')
 
@@ -16,6 +17,36 @@ app.get('/', (req, res) => {
     'heading': heading
     });  
 })
+
+app.get('/about', (req, res) => {
+  var title = 'My About Page';
+  var heading = 'The World of Animals';
+  res.render('pages/about', {
+    'title':title,
+    'heading': heading
+    });  
+})
+
+
+app.get('/contact', (req, res) => {
+  var title = 'My contact Page';
+  var heading = 'The World of Animals';
+  res.render('pages/contact', {
+    'title':title,
+    'heading': heading
+    });  
+})
+
+app.get('/users', (req, res) => {
+  var title = 'My Users Page';
+  var heading = 'The World of Animals';
+  res.render('users/index', {
+    'title':title,
+    'heading': heading,
+    'users': data
+    });  
+})
+
 
 app.get('/bird', (req, res) => {
   var title = 'My Favorite Birds';
@@ -44,6 +75,20 @@ app.get('/mammal', (req, res) => {
     });  
 })
 
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ var heading = 'The World of Animals';
+ res.render('users/view', {
+     title: title,
+   'heading': heading,
+     user: data[--id]
+ });
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(data);
 })
